699eba5e9f529adc1fd98a95a4f2c21a62a8a9da
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-06-29 08:35:24 +0530

    chore: practice advanced React concepts

---
+++ Cohort-3.0_ai_powered_full_stack_developer/react/script.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+let root = document.querySelector("#root");
+
+const h1 = React.createElement(
+  "h1",
+  {},
+  "Hello this is from react"
+);
+
+console.log(hoooo);
+
+ReactDOM.createRoot(root).render(h1);
